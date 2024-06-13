668f8bfacc99737c3790a9c8fe9b8b36c2aeabe2
Leandro <levsistemasit@gmail.com>
2024-06-12 21:37:21 -0300

    Updates API Pikachu

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ app.use(express.json());
 // Servir archivos estáticos desde la carpeta "public"
 app.use(express.static('public'));
 
+//app.set('views','./views/siguiente.html')
+
 app.get('/pokemon/:name', async (req, res) => {
     const pokemonName = req.params.name.toLowerCase();
 
     try {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         const pokemonData = response.data;
+        console.log(response.data.weight);
         res.send(`
         <!DOCTYPE html>
         <html lang="en">
@@ -26,12 +29,15 @@ app.get('/pokemon/:name', async (req, res) => {
             <link rel="stylesheet" href="/css/estilo.css">
         </head>
         <body>
-            <h1>${pokemonData.name.toUpperCase()}</h1>
-            <!-- <h1>pokemon</h1> -->
-            <img style="width: 200px;height: 200px;" src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
-            <p style="font-weight: bold;" >Height: ${pokemonData.height}</p>
-            <p>Weight: ${pokemonData.weight}</p>
-            <p>Type: ${pokemonData.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>  
+            <div>
+                <h1>${pokemonData.name.toUpperCase()}</h1>
+                <!-- <h1>pokemon</h1> -->
+                <img style="width: 200px;height: 200px;" src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
+                <p style="font-weight: bold;" >Height: ${pokemonData.height}</p>
+                <p>Weight: ${pokemonData.weight}</p>
+                <p>Type: ${pokemonData.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+                <a href='/siguiente.html' onclick="localStorage.setItem('lastPokemon', '${pokemonData.name}')">SIGUIENTE</a>
+            </div>
         </body>
         </html>
         `);
